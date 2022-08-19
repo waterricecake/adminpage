6591099a20c36579ed8a0a2d98b7ca8204c242df
watterricecake <fruturum@nate.com>
2022-08-19 18:49:03 +0900

    20220819

--- study/src/main/java/com/example/study/controller/model/SearchParam.java
+++ study/src/main/java/com/example/study/controller/model/SearchParam.java
@@ -1,33 +1,18 @@
 package com.example.study.controller.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SearchParam {
 
     private String account;
     private String email;
     private int page;
 
+
+
     //{"account":"","email":"","page":0}
-    public String getAccount() {
-        return account;
-    }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 }
